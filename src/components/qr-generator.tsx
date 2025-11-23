d79b4527f6ecf6def91b19c74918e0f89bfb4926
Sanjay <sanjayzingle@gmail.com>
2025-11-23 07:00:49 +0000

    Try fixing this error: `Console Error: Error: React is not defined. Erro

--- src/components/qr-generator.tsx
+++ src/components/qr-generator.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useForm, FormProvider } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import QRCode from 'qrcode';
